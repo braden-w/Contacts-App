1aadd662f4ad8a338d88307a70f1c367f0799896
Braden <bmw02002turbo@gmail.com>
2021-04-04 14:39:51 -0700

    Cleanup view

--- layouts/default.vue
+++ layouts/default.vue
@@ -47,18 +47,6 @@
     <v-main>
       <nuxt id="nuxt" />
     </v-main>
-    <!--
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    -->
     <v-footer :absolute="!fixed" app>
       <span>&copy; {{ new Date().getFullYear() }}</span>
     </v-footer>
