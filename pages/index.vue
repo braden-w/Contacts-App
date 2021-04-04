63091a21f1b7f653af86e10b14e83f776c505038
Braden <bmw02002turbo@gmail.com>
2021-04-04 15:28:16 -0700

    Add v-for for columns

--- pages/index.vue
+++ pages/index.vue
@@ -2,12 +2,10 @@
   <v-app class="dashboard">
     <v-container class="my-5">
       <v-row>
+        <v-col v-for="column in layout" :key="column.name">{{
+          column.name
+        }}</v-col>
         <v-col><v-icon>mdi-account</v-icon></v-col>
-        <v-col>Name</v-col>
-        <v-col>Email</v-col>
-        <v-col>Phone</v-col>
-        <v-col>Birthday</v-col>
-        <v-col>Last Contact</v-col>
       </v-row>
 
       <h1 class="subtitle-1 grey--text">Dashboard</h1>
