0190bd5d765e40b80713ef935887d7cbe6b350dd
Braden <bmw02002turbo@gmail.com>
2021-04-04 16:32:05 -0700

    Feat: Created onAuthStateChangedAction Mutation
    Responsible for handling Google Logins

--- store/auth.js
+++ store/auth.js
@@ -15,3 +15,44 @@ export const mutations = {
     state.signingIn = bool
   },
 }
+
+export const actions = {
+  async onAuthStateChangedAction({ commit, dispatch }, { authUser }) {
+    if (!authUser) {
+      // Perform logout operations
+    } else {
+      // Perform login operations
+      console.log(authUser)
+      const {
+        uid: userID,
+        email,
+        emailVerified,
+        photoURL,
+        displayName,
+      } = authUser
+      const emailDomain = email.split('@')[1]
+      const request = await axios.get(
+        `http://universities.hipolabs.com/search?domain=${emailDomain}`
+      )
+      const university = request.data[0]
+      const payload = {
+        userID,
+        email,
+        emailVerified,
+        photoURL,
+        displayName,
+        university: university.name,
+      }
+      try {
+        commit('setUser', payload)
+        dispatch('bindUserDataBetweenFirestoreAndVuex', userID, { root: true })
+        await this.$fire.firestore
+          .collection('users')
+          .doc(userID)
+          .set(payload, { merge: true })
+      } catch (err) {
+        alert(err)
+      }
+    }
+  },
+}
