cbab4550b3aaa25e2f68ee54585d06381d5594f6
Braden <bmw02002turbo@gmail.com>
2021-04-04 15:37:23 -0700

    Refactor: use "contact" instead of "project"

--- pages/index.vue
+++ pages/index.vue
@@ -12,29 +12,29 @@
       <h1 class="subtitle-1 grey--text">Dashboard</h1>
       <v-divider></v-divider>
 
-      <v-container fluid
-        ><v-card v-for="project in projects" :key="project.title" class="pa-3">
-          <v-row :class="`project ${project.status}`">
+      <v-container fluid>
+        <v-card v-for="contact in projects" :key="contact.title" class="pa-3">
+          <v-row :class="`contact ${contact.status}`">
             <v-col cols="12" md="6">
               <div class="caption grey--text">Project Title</div>
-              <div>{{ project.title }}</div>
+              <div>{{ contact.title }}</div>
             </v-col>
             <v-col cols="6" md="2">
               <div class="caption grey--text">Person</div>
-              <div>{{ project.person }}</div>
+              <div>{{ contact.person }}</div>
             </v-col>
             <v-col cols="6" md="2">
               <div class="caption grey--text">Due</div>
-              <div>{{ project.due }}</div>
+              <div>{{ contact.due }}</div>
             </v-col>
             <v-col cols="22" md="2">
               <div id="chips-container" class="right">
                 <v-chip
                   small
                   my-auto
-                  :class="`${project.status} caption white--text `"
+                  :class="`${contact.status} caption white--text `"
                 >
-                  {{ project.status }}
+                  {{ contact.status }}
                 </v-chip>
               </div>
             </v-col>
@@ -57,44 +57,20 @@ export default {
         { name: 'Birthday', columnsWide: 2 },
         { name: 'Last Contact', columnsWide: 2 },
       ],
-      projects: [
+      contacts: [
         {
-          title: 'Design a new website',
-          person: 'Braden Wong',
-          due: '1st Jan 2019',
-          status: 'ongoing',
-          content:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!',
-        },
-        {
-          title: 'Code up the homepage',
-          person: 'Jason',
-          due: '10th Jan 2019',
-          status: 'complete',
-          content:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!',
-        },
-        {
-          title: 'Design video thumbnails',
-          person: 'Ruobin',
-          due: '20th Dec 2018',
-          status: 'complete',
-          content:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!',
-        },
-        {
-          title: 'Create a community forum',
-          person: 'Sam',
-          due: '20th Oct 2018',
-          status: 'overdue',
-          content:
-            'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!',
+          image: '',
+          firstName: 'Braden',
+          lastName: 'Wong',
+          email: 'braden.wong@yale.edu',
+          phone: '6266274599',
+          lastContact: 'August 8',
         },
       ],
       sorts: [
-        { icon: 'mdi-folder', title: 'project name', query: 'title' },
+        { icon: 'mdi-folder', title: 'contact name', query: 'title' },
         { icon: 'mdi-account', title: 'person', query: 'person' },
-        { icon: 'mdi-folder', title: 'project status', query: 'status' },
+        { icon: 'mdi-folder', title: 'contact status', query: 'status' },
       ],
     }
   },
