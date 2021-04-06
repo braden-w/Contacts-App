198f2bd4aabbae8f73848d95f56d6c9d215701e8
Braden <bmw02002turbo@gmail.com>
2021-04-06 10:25:07 -0700

    Fix: Remove University from auth

--- store/auth.js
+++ store/auth.js
@@ -31,18 +31,12 @@ export const actions = {
         photoURL,
         displayName,
       } = authUser
-      const emailDomain = email.split('@')[1]
-      const request = await axios.get(
-        `http://universities.hipolabs.com/search?domain=${emailDomain}`
-      )
-      const university = request.data?.[0]?.name ?? ''
       const payload = {
         userID,
         email,
         emailVerified,
         photoURL,
         displayName,
-        university,
       }
       try {
         commit('setUser', payload)
